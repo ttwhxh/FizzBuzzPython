bf0fd7709f2fb8e8194e183e347665d7780a0920
ttwhxh <46936039+ttwhxh@users.noreply.github.com>
2021-03-12 21:18:26 -0600

    Add files via upload
    iii. cleaner code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,29 +1,20 @@
 from sys import exit
 
-i=1
+i = 1
 
-while i<100:
+while i<101:
 
 	if i%3==0 and i%5==0:
                 print 'FizzBuzz' 
 
-	#prints 'FizzBuzz' for multiples of 3 and 5
-	
 	elif i%3==0:
 		print 'Fizz' 
-	
-	#prints 'Fizz' for multiples of 3
-	
+		
 	elif i%5==0:
 		print 'Buzz' 
 
-	#prints 'Buzz' for multiples of 5
-	
 	else:
 		print i 
 	
-	#prints 'i', which will be the values that are not multiples of 3 and/or 5
-	
 	i=i+1
-
 exit()
